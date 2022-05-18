93b3d00d5c26ea8454c70b80a7a000b08c8a187a
Madusanka-w <mwattale@gmail.com>
2022-05-18 22:36:01 +0530

    initial commit

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,6 +4,8 @@ function Home() {
     return (
         <div>
             <h1 className="text-center">Home Page</h1>
+            <h1>Hellow Sri Lankaaaaaaaaaaa</h1>
+            
         </div>
     )
 }
