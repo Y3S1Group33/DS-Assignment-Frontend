3c81e523b20273a404359fb0f1e36ce1c8562b5b
Madusanka-w <mwattale@gmail.com>
2022-05-18 23:40:48 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import AdminPanel from "./pages/adminPanel"
 import AdminReservations from "./pages/adminPages/Reservations/adminReservations"
 import AdminReservationsUpdate from "./pages/adminPages/Reservations/updateReservations"
 import PaymentDashboard from "./pages/adminPages/Payments/PaymentDashboard";
-import PaymentDelete from "./pages/adminPages/Payments/PaymentDelete";
 import LocalPaymentGateway from "./pages/clientPages/LocalPaymentGateway";
 import StripePaymentGateway from "./pages/clientPages/StripePaymentGateway";
 
@@ -39,6 +38,7 @@ function App() {
             <Route exact path="/payments" element={<PaymentDashboard />} />
             <Route exact path="/payments/local" element={<LocalPaymentGateway />} />
             <Route exact path="/payments/stripe" element={<StripePaymentGateway />} />
+            <Route exact path="/home" element={<Home />} />
         </Routes>
       </Router>
 </div>
