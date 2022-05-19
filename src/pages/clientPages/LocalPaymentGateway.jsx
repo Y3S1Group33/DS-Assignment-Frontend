388bfdb19208d36a30108ac5b07900c5c1041bb9
Anjana Samarakoon <anjana.dinethra@gmail.com>
2022-05-19 13:03:09 +0530

    email service added

--- src/pages/clientPages/LocalPaymentGateway.jsx
+++ src/pages/clientPages/LocalPaymentGateway.jsx
@@ -1,7 +1,11 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
+import emailjs from '@emailjs/browser';
 import "../../CSS/payment.css";
 import axios from "axios";
 const LocalPaymentGateway = () => {
+    const form = useRef();
+
+    const[email,updateEmail]=useState("")
     const[holderName,updateHolderName]=useState("")
     const[cardNumber,updateCardNumber]=useState("")
     const[cvc,updateCvc]=useState("")
@@ -14,25 +18,37 @@ const LocalPaymentGateway = () => {
         expDate:expDate
     }
 
+
     const SetPayments=(e)=>{
         e.preventDefault()
 
 
-        axios.post("http://localhost:8080/payments",requestBody).then(res=>{
-            console.log(res.data)
-            //setCardHolderName(res.data.cardHolderName)
-        }).catch(err=>{
-            console.log(err)
-        })
+        // axios.post("http://localhost:8080/payments",requestBody).then(res=>{
+        //     console.log(res.data)
+        //     //setCardHolderName(res.data.cardHolderName)
+        // }).catch(err=>{
+        //     console.log(err)
+        // })
+
+        emailjs.sendForm('service_js8nvc9', 'template_yabaem9', form.current, '1asUfQ-WtKRiuysnb')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
     }
 
     return (
 
         <div>
             <div className="wrapper">
-                <form className="form-signin">
+                <form ref={form} className="form-signin">
                     <h2 className="form-signin-heading text-center">Payments Form</h2>
-                    <input type="text" className="form-control" placeholder="Card Holder Name" onChange={(e)=>{
+                    <input type="email" name="email" className="form-control" placeholder="Email" onChange={(e)=>{
+                        updateEmail(e.target.value)
+                    }} required=""
+                           autoFocus=""/>
+                    <input type="text" name="cardHolderName" className="form-control" placeholder="Card Holder Name" onChange={(e)=>{
                         updateHolderName(e.target.value)
                     }} required=""
                            autoFocus=""/>
